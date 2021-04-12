246569812b92efe2eb679c443e1bad37829731d3
Anett DAVID <davidanett@gmail.com>
2021-04-12 15:10:19 +0200

    Add a seperate function to display users

--- application/assets/js/script.js
+++ application/assets/js/script.js
@@ -1,23 +1,28 @@
 $(function () {
   const siteURL = $("#site-url").val()
+
+  const displayUsers = users => {
+    $("#users").html("")
+    $("#message").html("")
+    users.map(user => {
+      const date = new Date(user.created_at)
+      $("#users").append(`
+          <tr>
+              <td>${user.first_name}</td>
+              <td>${user.email}</td>
+              <td>${date.toLocaleDateString()}</td>
+          </tr>
+      `)
+    })
+  }
+
   const getUsers = (email = "") => {
     let url = `${siteURL}users/show?email=${email}`
     $.get(url, data => {
       const response = $.parseJSON(data)
       if (!response.error) {
         if (response.users.length) {
-          $("#users").html("")
-          $("#message").html("")
-          response.users.map(user => {
-            const date = new Date(user.created_at)
-            $("#users").append(`
-              <tr>
-                  <td>${user.first_name}</td>
-                  <td>${user.email}</td>
-                  <td>${date.toLocaleDateString()}</td>
-              </tr>
-          `)
-          })
+          displayUsers(response.users)
         } else {
           $("#message").html(`Pas d'utilisateur trouvé avec l'adresse mail '${email}'`)
         }
